5430ffe5b165d8796e5fc571e96e073f9a7f5827
RoyaJafari <jafarir.ir@gmail.com>
2018-12-11 23:21:11 +0330

    Updates

---
+++ precache-manifest.b9d37be00cf16104920f343ce3e09547.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d13ac1fe2839020a0ab",
+    "url": "/maktabkhoone-resume/static/css/main.aaad5aa5.chunk.css"
+  },
+  {
+    "revision": "2d13ac1fe2839020a0ab",
+    "url": "/maktabkhoone-resume/static/js/main.2d13ac1f.chunk.js"
+  },
+  {
+    "revision": "55ff70cce89d73451890",
+    "url": "/maktabkhoone-resume/static/js/1.55ff70cc.chunk.js"
+  },
+  {
+    "revision": "390924201790ae1f744c",
+    "url": "/maktabkhoone-resume/static/js/runtime~main.39092420.js"
+  },
+  {
+    "revision": "9a3da56dba5e6ea2ad30aa3e189e8326",
+    "url": "/maktabkhoone-resume/index.html"
+  }
+];
